060100ca1bb0bc425491139f03db9067d5230618
Justin113D <Justin113D@gmail.com>
2025-08-17 09:54:05 +0200

    Updated version to 1.0.1

--- publishAll.bat
+++ publishAll.bat
@@ -31,7 +31,7 @@ cd ..
 
 cd .\Releases
 
-set version="1.0.0"
+set version="1.0.1"
 
 tar --strip-components 1 -acf HedgeMatEdit-%version%-linux-arm64.zip -C .\linux-arm64 .
 tar --strip-components 1 -acf HedgeMatEdit-%version%-linux-x64.zip -C .\linux-x64 .
